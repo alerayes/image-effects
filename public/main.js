1c2600c8a92a9167e2189c0d8781a97972c5f003
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-12-12 21:26:03 -0300

    Implements file reading

--- public/main.js
+++ public/main.js
@@ -1,1 +1,19 @@
-console.log('test');
+async function init() {
+    const input = document.getElementById('upload')
+    const fileReader = new FileReader()
+  
+    fileReader.onloadend = () => {
+      let base64 = fileReader.result.replace(
+        /^data:image\/(png|jpeg|jpg);base64,/, ''
+      )
+
+      console.log(base64)
+      console.log(input.files[0])
+    }
+  
+    input.addEventListener('change', () => {
+      fileReader.readAsDataURL(input.files[0])
+    })
+  }
+  
+  init()
